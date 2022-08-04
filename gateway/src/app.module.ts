785ea3d34b309cfa9a229ae1dae235ef9f197b9e
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-08-04 17:34:36 +0700

    Adding microservice function

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,6 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+   ClientsModule.register([
+      {
+         name: 'TODO_SERVICE',
+         transport: Transport.TCP,
+         options: {
+            host: '127.0.0.1',
+            port: 8001,
+         },
+      },
+      {
+         name: 'USER_SERVICE',
+         transport: Transport.TCP,
+         opstions: {
+            host: '127.0.0.1',
+            port: 8002,
+         },
+      },
+   ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
