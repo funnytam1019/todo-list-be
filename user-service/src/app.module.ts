f986d4efe8219bf0abb6ff8abef84fdca8dbcbe5
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-08-04 17:35:56 +0700

    Adding user folder

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
    imports: [
       MongooseModule.forRoot('mongodb://localhost:27017/nest_user', {
          autoCreate: true
-      })
+      }),
+      UserModule
    ],
 })
 export class AppModule {}
