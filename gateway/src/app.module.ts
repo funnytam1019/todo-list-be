67400f1b3ecc22cca6008562adb2cf780519fed0
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-08-04 17:34:36 +0700

    Adding dto files

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -7,21 +7,13 @@ import { AppController } from './app.controller';
   imports: [
    ClientsModule.register([
       {
-         name: 'TODO_SERVICE',
+         name: 'USER_SERVICE',
          transport: Transport.TCP,
          options: {
             host: '127.0.0.1',
-            port: 8001,
-         },
-      },
-      {
-         name: 'USER_SERVICE',
-         transport: Transport.TCP,
-         opstions: {
-            host: '127.0.0.1',
             port: 8002,
          },
-      },
+      }
    ]),
   ],
   controllers: [AppController],
