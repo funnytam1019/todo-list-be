f5aa7a6872b769602bab4a25c229f0d695564a5a
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-08-04 17:35:56 +0700

    Adding dto files

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,53 +1,13 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createUserRequest } from './create-user-request.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-  
-  //#API orderService - create order
-  @Post('order/create')
-  createOrder(
-    @Body('name') orderName: string,
-    @Body('phone') orderPhone: string,
-    @Body('price') orderPrice: number
-  ){
-    return this.appService.createOrder(orderName, orderPhone, orderPrice);
-  }
-  
-  //#API orderService - get specific order details
-  @Get('order/find/:id')
-  findOrder(
-    @Param('id') paramOrderId: number
-  ){
-    return this.appService.findOrder(paramOrderId);
-  }
-
-  //#API orderService - get all orders
-  @Get('order/all')
-  allOrder(){
-    return this.appService.allOrder();
-  }
-
-  //#API orderService - cancel order
-  @Post('order/cancel')
-  cancelOrder(
-    @Body('id') orderId: number
-  ){
-    return this.appService.cancelOrder(orderId);
-  }
-
-  //#API orderService - confirm order, pay order & update status delivery
-  @Post('order/pay')
-  payOrder( 
-    @Body('id') orderId: number
-  ){
-    return this.appService.payOrder(orderId);
-  }
-
-  //#API orderService - get all orders
-  @Get('pay/all')
-  allPayment(){
-    return this.appService.allPayment();
-  }
+   constructor(private readonly appService: AppService) {}
+   
+   @Post()
+   createUser(@Body() createUserRequest: createUserRequest) {
+         
+   }
 }
---
+++ gateway/src/create-user-request.dto.ts
@@ -0,0 +1,4 @@
+export class createUserRequest {
+   email: string;
+   password: string;
+}
